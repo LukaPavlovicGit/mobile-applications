e71ccec493cd9f4bb5caf066b2bce84ebdf2f685
LukaPavlovicGit <lukapavlovic032@gmail.com>
2023-03-07 22:57:14 +0100

    impelemented InProgressTicketsFragment and DoneTicketsFragment, and its Recycler Views

--- RafJira/app/src/main/java/com/example/view/fragments/InProgressTicketsFragment.java
+++ RafJira/app/src/main/java/com/example/view/fragments/InProgressTicketsFragment.java
@@ -1,6 +1,55 @@
 package com.example.view.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.raf_jira.databinding.FragmentInprogressTicketsBinding;
+import com.example.raf_jira.databinding.FragmentTodoTicketsBinding;
+import com.example.view.viewPager.TicketAdapter;
+import com.example.viewModels.SharedViewModel;
 
 public class InProgressTicketsFragment extends Fragment {
+
+    private FragmentInprogressTicketsBinding binding;
+    private SharedViewModel sharedViewModel;
+    private TicketAdapter adapter;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentInprogressTicketsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+        initRecyclerView();
+        initObservers();
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = binding.toDoRecyclerview;
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireActivity(), LinearLayoutManager.VERTICAL, false));
+        recyclerView.setHasFixedSize(true);
+
+        adapter = new TicketAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initObservers(){
+        sharedViewModel.getInProgressTicketsLiveData().observe(getViewLifecycleOwner(), tickets -> {
+            adapter.setTickets(tickets);
+        });
+    }
 }
